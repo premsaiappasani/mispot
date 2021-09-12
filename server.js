af304d8cb1757e5a7110767705c7f7abf785787c
Prem Sai <premsaiappasani@gmail.com>
2021-09-12 23:41:26 +0530

    working model done

--- server.js
+++ server.js
@@ -7,30 +7,27 @@ app.use(express.json());
 
 
 
+
 app.use(express.urlencoded({
   extended: true
 }));
 
 
 let arr=[0,0,0,0];
-let brr=['bottle','pen','cell phone','hand'];
+let brr=['bottle','pen','cell phone','remote'];
 app.set('view engine', 'ejs');
+
+
+app.get("/home",(req,res)=>{
+    res.render('home');
+});
 
 
 app.get("/products",(req,res)=>{
     console.log("req recieved");
     let obj={arr,brr};
-    res.render("verify",obj);
+    res.render("products",obj);
 });
-
-
-app.post("/products",(req,res)=>{
-    console.log("info recieved");
-    let str=req.body.str;
-    brr[1]=str;
-    res.send("done");
-});
-
 
 
 app.post("/verify/:tagid",(req,res)=>{
@@ -38,8 +35,8 @@ app.post("/verify/:tagid",(req,res)=>{
     let id=req.params.tagid;
     //res.render("/sample.html",{id,str});
     arr[id]=1;
-    res.send("done!");
     console.log(arr);
+    res.redirect('http://localhost:8080/products/');
 });
 
 
@@ -48,7 +45,7 @@ app.get("/verify/:tagid",(req,res)=>{
     let id2=parseInt(id);
     let str=brr[id2];
     let obj={id,str};
-    res.render('home',obj);
+    res.render('verify',obj);
 });
 
 
@@ -60,17 +57,6 @@ app.get("/success",(req,res)=>{
 
 
 
-
-app.post("/", (req,res)=>{
-    let n1= parseInt(req.body.n1);
-    let n2= parseInt(req.body.n2);
-    let n3=(n1/n2);
-    n3/=n2;
-    n3*=10000;
-    n3=n3.toFixed(3);
-    console.log(n3);
-    res.send("BMI "+n3);
-});
 app.listen(8080,()=>{
-    console.log("Hello World")
+    console.log("Listening");
 });
